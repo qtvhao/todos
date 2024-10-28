b65f6a62ed7edbb31cdfc4d5d571f5bbb9ddcb12
Your Name <you@example.com>
2024-10-28 13:11:49 +0100

    Add modules for todos, auth, and notifications

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [TodosModule, AuthModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
