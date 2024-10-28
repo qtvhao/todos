44459452379c06b09e97ea30bc53657e3c53bc8c
Your Name <you@example.com>
2024-10-28 13:16:08 +0100

    Add todos controller and its test

--- app/src/todos/todos.module.ts
+++ app/src/todos/todos.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { TodosController } from './todos.controller';
 
 @Module({
-  providers: [TodosService]
+  providers: [TodosService],
+  controllers: [TodosController]
 })
 export class TodosModule {}
