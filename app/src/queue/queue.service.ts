b8abd25c6ef1714bc304c2ce1d6cf9b6c17f863b
Your Name <you@example.com>
2024-10-30 08:50:38 +0100

    Refactor addJob method in QueueService to log job data and queue name before adding the job

--- app/src/queue/queue.service.ts
+++ app/src/queue/queue.service.ts
@@ -7,7 +7,8 @@ const BULL_QUEUE_NAME = process.env.BULL_QUEUE_NAME || 'queue';
 @Injectable()
 export class QueueService {
     constructor( @InjectQueue(BULL_QUEUE_NAME) private readonly queue: any) {}
-    async addJob(job: any) {
-        return this.queue.add(job);
+    async addJob(jobData: any) {
+        console.log('Creating todo with job data:', jobData, 'in queue:', this.queue.name);
+        return this.queue.add(jobData);
     }
 }
