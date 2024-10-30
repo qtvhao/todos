4f989aff3b4f69fcaa1da64eae4bee77562041c2
Your Name <you@example.com>
2024-10-30 08:24:47 +0100

    Refactor queue module to use dynamic redis port

--- app/src/queue/queue.module.ts
+++ app/src/queue/queue.module.ts
@@ -10,10 +10,11 @@ import { QueueService } from './queue.service';
 const BULL_QUEUE_NAME = process.env.BULL_QUEUE_NAME || 'queue';
 const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || '';
+const REDIS_PORT = process.env.REDIS_PORT || '6389';
 // 
-let url = `redis://${REDIS_PASSWORD}@${REDIS_HOST}:6389`;
+let url = `redis://${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`;
 if (REDIS_PASSWORD.length === 0) {
-  url = `redis://${REDIS_HOST}:6389`;
+  url = `redis://${REDIS_HOST}:${REDIS_PORT}`;
 }
 @Module({
   imports: [
