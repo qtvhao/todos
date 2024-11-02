0c9dfe43c611ecab913005fe945ce1c133ec8986
willwilson <qtvhao@gmail.com>
2024-11-02 14:02:51 +0700

    Update WS_URL constant to use API_URL for WebSocket connection

--- chat/src/constants.js
+++ chat/src/constants.js
@@ -2,4 +2,4 @@
 export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 export const FETCH_MESSAGES_ENDPOINT = `${API_URL}/messages`;
 export const SEND_MESSAGE_ENDPOINT = `${API_URL}/todos`;
-export const WS_URL = process.env.REACT_APP_WS_URL || 'ws://localhost:3000';
+export const WS_URL = API_URL;
