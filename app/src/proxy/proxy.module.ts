d70b25b7c59349a43459e81cfb9fb31f5a9a64ee
Your Name <you@example.com>
2024-11-03 04:30:23 +0100

    Implement proxying for /users requests in ProxyModule using express-http-proxy

--- app/src/proxy/proxy.module.ts
+++ app/src/proxy/proxy.module.ts
@@ -1,11 +1,20 @@
-import { ProxyMiddleware } from './proxy.middleware';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import * as proxy from 'express-http-proxy';
 
 @Module({})
 export class ProxyModule implements NestModule {
+    private readonly targetUrl = 'http://memory-permissions.system-production';
     configure(consumer: MiddlewareConsumer) {
-        consumer
-            .apply(ProxyMiddleware)
-            .forRoutes('/users'); // Apply to paths starting with /users
-    }
+    // Proxy all requests starting with /api to the target API
+    consumer
+      .apply(
+        proxy(this.targetUrl, {
+          proxyReqPathResolver: (req: any) => {
+            // Forward the request path and query parameters
+            return `/users${req.url}`;
+          },
+        }),
+      )
+      .forRoutes('/users'); // This will route all /users/* requests to the proxy
+  }
 }
