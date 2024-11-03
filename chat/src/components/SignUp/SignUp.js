f9315f08cc0decc615984c808102840a6bc9f78b
willwilson <qtvhao@gmail.com>
2024-11-03 10:45:48 +0700

    Add Sign Up functionality and link in Login component

---
+++ chat/src/components/SignUp/SignUp.js
@@ -0,0 +1,44 @@
+// src/components/SignUp/SignUp.js
+import React, { useState } from 'react';
+import { useAuth } from '../../context/AuthContext';
+import './SignUp.css';
+import { useNavigate } from 'react-router-dom';
+
+const SignUp = () => {
+    const { signUp, createAccessKeys, login } = useAuth(); // Assuming you have a signUp function in AuthContext
+    const [username, setUsername] = useState('');
+    const [signUpError, setSignUpError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let signedUp = await signUp(username);
+        if (!signedUp) {
+            console.error('Failed to sign up');
+            setSignUpError('Failed to sign up');
+            return;
+        }
+        let { access_key_id, secret_access_key } = await createAccessKeys(username);
+
+        login(access_key_id, secret_access_key);
+        navigate('/chat'); // Redirect after sign up
+    };
+
+    return (
+        <div className="signup-container">
+            <form className="signup-form" onSubmit={handleSubmit}>
+                <h2>Sign Up</h2>
+                {signUpError && <p className="error">{signUpError}</p>}
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUp;
