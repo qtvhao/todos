4615f00711d6cd4d4dd17959bc784e920fa5fb54
willwilson <qtvhao@gmail.com>
2024-11-14 17:38:36 +0700

    Update ALIGN_TOKENS_ENDPOINT to point to the correct todos service URL

--- chat/src/constants.js
+++ chat/src/constants.js
@@ -6,5 +6,5 @@ export const SEND_MESSAGE_ENDPOINT = `${API_URL}/todos`;
 export const WS_URL = API_URL;
 export const STATIC_URL = process.env.REACT_APP_API_URL + '/static/';
 
-export const ALIGN_TOKENS_ENDPOINT = `https://http-align-partnerapis-production-80.schnworks.com/stablize`;
+export const ALIGN_TOKENS_ENDPOINT = `https://http-align-partnerapis-production-80.schnworks.com/todos`;
 export const WS_ALIGN_TOKEN_URL = process.env.REACT_APP_WS_ALIGN_TOKEN_URL || 'http://localhost:3000';
