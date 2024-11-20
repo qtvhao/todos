4ce40c3c276a8ce006b5f54295ea9f22a0b4d06b
willwilson <qtvhao@gmail.com>
2024-11-20 21:59:49 +0700

    Refactor WebSocketProvider to use setupWebSocket for improved connection handling and remove unused socket event listeners

--- chat/src/WebSocketProvider/WebSocketProvider.js
+++ chat/src/WebSocketProvider/WebSocketProvider.js
@@ -1,8 +1,7 @@
 // src/WebSocketProvider.js
 import React, { createContext, useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { connectWebSocket } from '../services/webSocketService';
-import { WS_URL, ALIGN_TOKENS_ENDPOINT, WS_ALIGN_TOKEN_URL, } from '../constants';
+import { WS_URL, WS_ALIGN_TOKEN_URL, } from '../constants';
 import { useMessages } from '../context/MessageContext';
 import { setupWebSocket } from '../WebSocketProvider/webSocketUtils'
 import { handleNotification, handleJobResult, handleDisconnect } from '../WebSocketProvider/webSocketHandlers';
@@ -35,52 +34,15 @@ const WebSocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (auth) { //
-      let socket = connectWebSocket(WS_ALIGN_TOKEN_URL, auth.token);
-      socket.on('notification', (message) => {
-        console.log('Received message:', message);
+      const alignSocket = setupWebSocket(WS_ALIGN_TOKEN_URL, auth.token, {
+        notification: handleNotification,
+        job_result: (message) => { handleJobResult(message, addAssistantMessage) },
+        disconnect: handleDisconnect
       });
-      socket.on('disconnect', (reason) => {
-        if (reason === 'io server disconnect') {
-          console.log('Disconnected:', reason);
-          alert('Disconnected from server. Please signup again.');
-          window.location.href = '/signup';
-        }
-        console.log('Disconnected:', reason);
-      });
-      socket.on('job_result', (message) => {
-        message = JSON.parse(message);
-        console.log('Received message:', Object.keys(message));
-        let result = message.result;
-        let audioUrl = result.audio_url;
-        let flat = result.flat;
-        let payload = {
-          "tokens_texts": flat,
-          "audio_file": audioUrl,
-        };
-        let text = `curl '${ALIGN_TOKENS_ENDPOINT}' \
--X 'POST' \
--H 'Accept: application/json' \
--H 'Content-Type: application/json' \
---data-binary $'${JSON.stringify(payload)}'`;
-        let {
-          active_thread_id,
-          tokens,
-        } = result;
-        let newMessage = {
-          audioFile: null,
-          formatted: null,
-          tokens,
-          text,
-          sender: 'Stablizer',
-          threadId: active_thread_id,
-          timestamp: Date.now()
-        };
-        console.log('New message (aligned_tokens):', newMessage);
-        addAssistantMessage(newMessage);
-      });
-      setWsAlignToken(socket);
+      setWsAlignToken(alignSocket);
+
       return () => {
-        socket.disconnect();
+        alignSocket.disconnect();
       };
     }
     // eslint-disable-next-line
